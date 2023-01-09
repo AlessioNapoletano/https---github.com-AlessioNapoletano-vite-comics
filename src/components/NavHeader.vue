0be08ee275007940e7629b9f6bec70879a74e8b1
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-09 17:05:06 +0100

    fix folder

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    data() {
+        return {
+            navList:
+                [
+                    {
+                        text: "CHARACTERS"
+                    }
+                ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header class="py-3">
+        <div class="container-lg d-flex justify-content-between">
+            <div class="image-fluid">
+                <img src="../assets/img/dc-logo.png" alt="DC comics Logo">
+            </div>
+
+            <nav>
+                <ul>
+                    <li>
+                        ciao
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    border: 1px solid black;
+}
+</style>
