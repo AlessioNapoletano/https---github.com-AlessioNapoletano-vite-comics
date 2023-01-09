6f288a9c8ec3f08ee4f792e07efecfa78b706e3b
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-09 16:56:28 +0100

    add component NavHeader

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,16 +1,19 @@
 <script>
+import NavHeader from './components/NavHeader.vue';
 
+export default {
+  components: {
+    NavHeader,
+  }
+}
 </script>
 
 <template>
+  <NavHeader />
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables' as *;
 @use 'bootstrap/scss/bootstrap.scss' as *;
-
-h1 {
-  color: $main-color;
-}
 </style>
---
+++ vite-comics/src/components/NavHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-2">
+                    <div class="image-fluid">
+                        <img src="../assets/img/dc-logo.png" alt="DC comics Logo">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    border: 1px solid black;
+}
+</style>
