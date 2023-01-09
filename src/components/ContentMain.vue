48b997579eec8b0861f7d6a1fd59d8493cb3d624
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-09 20:33:48 +0100

    modified component "ListLinkFooter"

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,14 +1,8 @@
-<script>
-export default {
-
-}
-</script>
-
 <template>
     <div id="content" class="d-flex align-items-center">
         <div class="container-lg">
             <h2 class="text-light">
-                -->Content here &lt;--
+                -->Content goes here &lt;--
             </h2>
         </div>
 
@@ -22,5 +16,9 @@ export default {
 #content {
     background-color: $secondary-color;
     height: 150px;
+
+    h2 {
+        font-weight: 600;
+    }
 }
 </style>
--- src/components/ListLinkFooter.vue
+++ src/components/ListLinkFooter.vue
@@ -1,5 +1,96 @@
 <script>
 export default {
+    data() {
+        return {
+            dcComicsList:
+                [
+                    {
+                        text: 'Characters',
+                    },
+                    {
+                        text: 'Comics',
+                    },
+                    {
+                        text: 'Movies',
+                    },
+                    {
+                        text: 'TV',
+                    },
+                    {
+                        text: 'Games',
+                    },
+                    {
+                        text: 'Videos',
+                    },
+                    {
+                        text: 'News',
+                    }
+                ],
+            shopList:
+                [
+                    {
+                        text: 'Shop DC',
+                    },
+                    {
+                        text: 'Shop DC Collectibles',
+                    }
+                ],
+            dcList:
+                [
+                    {
+                        text: 'Terms Of Use',
+                    },
+                    {
+                        text: 'Privacy policy (New)',
+                    },
+                    {
+                        text: 'Ad Choices',
+                    },
+                    {
+                        text: 'Advertising',
+                    },
+                    {
+                        text: 'Jobs',
+                    },
+                    {
+                        text: 'Subscriptions',
+                    },
+                    {
+                        text: 'Talent Workshops',
+                    },
+                    {
+                        text: 'CPSC Certofocates',
+                    },
+                    {
+                        text: 'Ratings',
+                    },
+                    {
+                        text: 'Shop Help',
+                    },
+                    {
+                        text: 'Contact Us',
+                    },
+                ],
+            sitesList:
+                [
+                    {
+                        text: 'DC',
+                    },
+                    {
+                        text: 'MAD Magazine',
+                    },
+                    {
+                        text: 'DC Kids',
+                    },
+                    {
+                        text: 'DC Universe',
+                    },
+                    {
+                        text: 'DC Power Visa',
+                    }
+                ]
+        }
+    }
 
 }
 </script>
@@ -9,50 +100,51 @@ export default {
         <div class="container-lg">
             <div class="row">
                 <!--col for link-->
-                <div class="col-5">
+                <div class="col-6">
                     <div class="row">
                         <div class="col-4">
+                            <!--DC Comics Link-->
                             <nav class="pt-4">
                                 <h4 class="mb-3">
                                     DC COMICS
                                 </h4>
 
                                 <ul>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
+                                    <li v-for="item in dcComicsList">
+                                        <a href="#">
+                                            {{ item.text }}
+                                        </a>
                                     </li>
                                 </ul>
                             </nav>
 
+                            <!--Shop Link-->
                             <nav>
                                 <h4>
                                     SHOP
                                 </h4>
 
                                 <ul>
-                                    <li>
-                                        item
+                                    <li v-for="item in shopList">
+                                        <a href="#">
+                                            {{ item.text }}
+                                        </a>
                                     </li>
-                                    <li>
-                                        item
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <div class="col-4">
+                            <nav class="pt-4">
+                                <h4 class="mb-3">
+                                    DC
+                                </h4>
+
+                                <ul>
+                                    <li v-for="item in dcList">
+                                        <a href="#">
+                                            {{ item.text }}
+                                        </a>
                                     </li>
                                 </ul>
                             </nav>
@@ -62,62 +154,14 @@ export default {
                         <div class="col-4">
                             <nav class="pt-4">
                                 <h4 class="mb-3">
-                                    DC COMICS
+                                    SITES
                                 </h4>
 
                                 <ul>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-
-                        <div class="col-4">
-                            <nav class="pt-4">
-                                <h4 class="mb-3">
-                                    DC COMICS
-                                </h4>
-
-                                <ul>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
-                                    </li>
-                                    <li>
-                                        item
+                                    <li v-for="item in sitesList">
+                                        <a href="#">
+                                            {{ item.text }}
+                                        </a>
                                     </li>
                                 </ul>
                             </nav>
@@ -126,7 +170,7 @@ export default {
                 </div>
 
                 <!--Col for img-->
-                <div class="col-7">
+                <div class="col-6">
                     <div class="image-fluid">
                         <img src="../assets/img/dc-logo-bg.png" alt="DC logo">
                     </div>
@@ -157,10 +201,11 @@ nav {
 
     ul {
         padding-left: 0;
+    }
 
-        li {
-            color: grey;
-        }
+    a {
+        text-decoration: none;
+        color: grey;
     }
 
 }
